3ed3cd6261b7353e91295dbd6627598b089e4d74
Eider-Caballero <eider.caballero@unavarra.es>
2026-05-05 17:07:37 +0200

    Update comando
    Cambio color

--- script.R
+++ script.R
@@ -5,6 +5,8 @@ dt <- iris
 
 # Un gráfico, 
 # pero ahora mejor grafico esto...
-ggplot(dt, aes(x = Sepal.Length, y = Petal.Width)) +
+ggplot(dt, aes(x = Sepal.Length, y = Petal.Width, color = "red")) +
   geom_point() +
   geom_smooth() 
+
+# editado por eider
